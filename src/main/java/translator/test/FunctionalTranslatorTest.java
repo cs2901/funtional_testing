6f7b002d02899874095eef7808192990c51c62df
jesubellido <jesubellido@gmail.com>
2019-06-27 12:48:20 -0500

    Spring Boot

--- src/main/java/translator/test/FunctionalTranslatorTest.java
+++ src/main/java/translator/test/FunctionalTranslatorTest.java
@@ -1,5 +1,6 @@
 package translator.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,12 +9,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FunctionalTranslatorTest {
-    @Test(invocationCount = 10, threadPoolSize = 5)
-    public void loadTest() {
+    @Test(invocationCount = 1, threadPoolSize = 1)
+    public void loadTest() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/jesus/Downloads/chromedriver");
         System.out.printf("%n[START] Thread Id : %s is started!",Thread.currentThread().getId());
         ChromeDriver driver = new ChromeDriver(DesiredCapabilities.chrome());
-        driver.get("http://localhost:8080/translator/EN/ES/HELLO");
+        driver.get("file:///Users/jesus/Documents/GitHubEducation/cs2901/funtional_testing/src/main/resources/translate.html");
+        driver.findElement(By.id("from")).sendKeys("value", "EN");
+        driver.findElement(By.id("to")).sendKeys("value", "ES");
+        driver.findElement(By.id("text")).sendKeys("value", "Hello World");
+        driver.wait(1000);
         System.out.printf("%n[END] Thread Id : %s",Thread.currentThread().getId());
         driver.quit();
 
