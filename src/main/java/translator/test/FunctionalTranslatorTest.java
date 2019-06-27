af57291f85de7016c2457a9d8d7e8958acda0cb7
jesubellido <jesubellido@gmail.com>
2019-06-27 17:15:20 -0500

    More test cases added

--- src/main/java/translator/test/FunctionalTranslatorTest.java
+++ src/main/java/translator/test/FunctionalTranslatorTest.java
@@ -1,5 +1,6 @@
 package translator.test;
 
+import com.google.common.io.Resources;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -10,25 +11,51 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
 
 public class FunctionalTranslatorTest {
-    @Test(invocationCount = 100, threadPoolSize = 5)
+
+    Map<String, String> dictionary = new HashMap<String, String>();
+    String driverPath = Resources.getResource("chromedriver").getPath();
+    String htmlPath = Resources.getResource("translate.html").getPath();
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+        String en2es = Resources.getResource("en_es.csv").getPath();
+        try (BufferedReader br = new BufferedReader(new FileReader(en2es))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                dictionary.put(values[0], values[1]);
+            }
+        }
+    }
+
+
+    @Test(invocationCount = 1, threadPoolSize = 1)
     public void loadTest() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "/Users/jesus/Downloads/chromedriver");
+        List<String> keysAsArray = new ArrayList<>(dictionary.keySet());
+        Random r = new Random();
+        String key = keysAsArray.get(r.nextInt(keysAsArray.size()));
+        System.setProperty("webdriver.chrome.driver", driverPath);
         System.out.printf("%n[START] Thread Id : %s is started!",Thread.currentThread().getId());
         ChromeDriver driver = new ChromeDriver(DesiredCapabilities.chrome());
-        driver.get("file:///Users/jesus/Documents/GitHubEducation/cs2901/funtional_testing/src/main/resources/translate.html");
+        driver.get("file://"+htmlPath);
         driver.findElement(By.id("from")).sendKeys("EN");
         driver.findElement(By.id("to")).sendKeys( "ES");
-        driver.findElement(By.id("text")).sendKeys("Hello World");
+        driver.findElement(By.id("text")).sendKeys(key);
         driver.findElement(By.id("btnTranslate")).click();
 
         try {
             WebDriverWait wait = new WebDriverWait(driver, 1);
             WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("translated")));
             String response = element.getAttribute("value");
-            Assert.assertEquals("Hola Mundo", response);
+            Assert.assertEquals(dictionary.get(key), response);
         }catch (Exception e){
             throw new TimeoutException("TIMEOUT");
 
